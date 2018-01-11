eed5e7e8009cb1ac3b62e807ffc39c3ed7b18964
Enrique Alvarez <ealvarezpereira@danielcastelao.org>
2018-01-11 09:16:54 +0100

    Prueba pull request contornos de desenvolvemento

---
+++ src/boletinpullrequest/BoletinPullRequest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletinpullrequest;
+
+/**
+ *
+ * @author quique
+ */
+public class BoletinPullRequest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+        System.out.println("Tú sabes si la hermeneutica telúrica incaica transtrueca de la peripatetica neoterica de la filosofía aristotélica,\n"
+                + "se ve influenciada por la inocuidad fáctica de los diálogos socráticos no dogmáticos?");
+    }
+    
+}
